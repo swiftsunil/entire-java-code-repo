7d9b45879a8728e96c92d01ac9812ea9657f73cd
Sunil Kumar <sunil.kumar2@pearson.com>
2014-10-30 07:11:42 -0600

    string manipulation

--- src/test/java/com/pearson/qa/ocv2/StringManipiulationCompleteTests.java
+++ src/test/java/com/pearson/qa/ocv2/StringManipiulationCompleteTests.java
@@ -1,6 +1,7 @@
-package com.pearson.qa.ocv2.StringBuilder;
-
+package com.pearson.qa.ocv2;
+import com.pearson.qa.ocv2.StringBuilder.StringBuilderClass;
 import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -10,35 +11,34 @@ import java.io.IOException;
 /**
  * Created by Sunil Kumar on 10/28/2014.
  */
+public class StringManipiulationCompleteTests {
+    final static String filename = "C:\\Users\\Sunil Kumar\\Desktop\\Tools and Languages\\" +
+            "Automation Installation and Training folder\\TRAINING PROJECTS\\ENTIRE JAVA CODE project\\" +
+            "entire-java-code-project\\FileReaderTextFile.txt";
+    Logger logger = Logger.getLogger(StringManipiulationCompleteTests.class);
 
-public class StringBuilderClass {
-    final static String filename = "C:\\Users\\Sunil Kumar\\Desktop\\Tools and Languages" +
-            "\\Automation Installation and Training folder\\" +
-            "TRAINING PROJECTS\\ESELENIUM GOOGLE SEARCH TEXT project\\" +
-            "sunil-eselenium-google\\FileReaderTextFile.txt";
-    Logger logger = Logger.getLogger(StringBuilderClass.class);
-
-    public static void main(String[] args) {
+    @Test
+    public  void validateStringManipulation() {
         // StringBuilder with 16 empty elements
         StringBuilder sb = new StringBuilder();
         sb.append("Hello from JCG");
-        System.out.println("sb appends a string: " + sb);
+        logger.debug("sb appends a string: " + sb);
         // append a character
         char c = '!';
         sb.append(c);
-        System.out.println("sb after appending a char: " + sb);
+        logger.debug("sb after appending a char: " + sb);
 
         sb.insert(6, "everyone ");
-        System.out.println("sb after insert: " + sb);
+        logger.debug("sb after insert: " + sb);
 
         // StringBulder with a initialized capacity
         StringBuilder sbnew = new StringBuilder(15);
         sbnew.append(123456789);
-        System.out.println("sb with length " + sbnew.length() + " and capacity " + sbnew.capacity() +
+        logger.debug("sb with length " + sbnew.length() + " and capacity " + sbnew.capacity() +
                 " appends an int: " + sbnew);
         // delete 234
         sbnew.delete(1, 4);
-        System.out.println("sb after delete: " + sbnew);
+        logger.debug("sb after delete: " + sbnew);
 
         // read from a file and append into a StringBuilder every new line
         try {
@@ -58,7 +58,7 @@ public class StringBuilderClass {
             // this string contains the character sequence
             String readFile = sbFile.toString();
             br.close();
-            System.out.println("from file: " + readFile);
+            logger.debug("from file: " + readFile);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
